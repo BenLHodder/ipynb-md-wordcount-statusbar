a41cd4cb0e2b991f78bdfd1cfa2a50e3904b5142
Ben Hodder <s4006398@student.rmit.edu.au>
2025-05-10 18:55:54 +1000

    Add activation events and implement word count display in status bar for markdown cells

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,86 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+let statusBarItem: vscode.StatusBarItem;
+
 export function activate(context: vscode.ExtensionContext) {
+   
+    // Create and show status bar item
+    statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
+    context.subscriptions.push(statusBarItem);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "ipynb-md-wordcount-statusbar" is now active!');
+    // Update status bar text
+    const updateWordCount = () => {
+        const notebookEditor = vscode.window.activeNotebookEditor;
+        const textEditor = vscode.window.activeTextEditor;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('ipynb-md-wordcount-statusbar.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from ipynb md wordcount statusbar!');
-	});
+        // If an .ipynb notebook is open
+        if (notebookEditor) {
+            const mdCells = notebookEditor.notebook.getCells().filter(cell =>
+                cell.kind === vscode.NotebookCellKind.Markup
+            );
+            const totalWords = mdCells.reduce((sum, cell) => {
+                const matches = cell.document.getText().match(/\b\w+\b/g);
+                return sum + (matches ? matches.length : 0);
+            }, 0);
 
-	context.subscriptions.push(disposable);
+            statusBarItem.text = `MD Words (Notebook): ${totalWords}`;
+            statusBarItem.show();
+            return;
+        }
+
+        // If a .md text file is open
+        if (textEditor && textEditor.document.languageId === 'markdown') {
+            const matches = textEditor.document.getText().match(/\b\w+\b/g);
+            const totalWords = matches ? matches.length : 0;
+
+            statusBarItem.text = `MD Words (File): ${totalWords}`;
+            statusBarItem.show();
+            return;
+        }
+
+        statusBarItem.hide();
+    };
+
+
+    // Event listeners for notebook/editor changes
+    context.subscriptions.push(
+        vscode.window.onDidChangeActiveNotebookEditor(updateWordCount),
+        vscode.workspace.onDidChangeNotebookDocument(event => {
+            const active = vscode.window.activeNotebookEditor;
+            if (active && event.notebook === active.notebook) {updateWordCount();}
+        }),
+        vscode.workspace.onDidChangeTextDocument(event => {
+            const notebookEditor = vscode.window.activeNotebookEditor;
+            const textEditor = vscode.window.activeTextEditor;
+
+            // Update if the change affects a markdown notebook cell
+            if (
+                notebookEditor &&
+                notebookEditor.notebook.getCells().some(cell => cell.document.uri.toString() === event.document.uri.toString())
+            ) {
+                updateWordCount();
+                return;
+            }
+
+            // Update if the change affects the current markdown text file
+            if (
+                textEditor &&
+                textEditor.document.uri.toString() === event.document.uri.toString() &&
+                textEditor.document.languageId === 'markdown'
+            ) {
+                updateWordCount();
+                return;
+            }
+        }),
+
+    );
+
+    // Initial count
+    updateWordCount();
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+    if (statusBarItem) {
+        statusBarItem.dispose();
+    }
+}
